916b6d6a7538f3b2369a5acca62d1e27cbe88fda
Rami Abo Ajeeb <rami@zeour.co.uk>
2024-10-31 16:02:55 +0400

    init commit | 31/10/2024

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,99 @@
+"use client"
+import Image from 'next/image'
+import React, { useState, useTransition } from 'react'
+import TabButton from './TabButton';
+
+
+const TAB_DATA = [
+    {
+        title:"Skills",
+        id:"skills",
+        content:(
+            <ul className='list-disc pl-2'>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+                <li>TailwindCSS</li>
+                <li>JavaScript</li>
+                <li>Ajax</li>
+                <li>JQuery</li>
+                <li>Node.js</li>
+                <li>React</li>
+                <li>Nextjs</li>
+                <li>Express</li>
+                <li>PHP</li>
+                <li>Laravel</li>
+                <li>Python</li>
+                <li>Mysql</li>
+                <li>MongoDB</li>
+                <li>PostgreSQL</li>
+            </ul>
+        )
+    },
+    {
+        title:"Education",
+        id:"education",
+        content:(
+            <ul className='list-disc pl-2'>
+                <li>Information Technology Engineering | AI Department</li>
+                <li>Tishreen University, Lattakia, Syria</li>
+            </ul>
+        )
+    },
+    {
+        title:"Experience",
+        id:"experience",
+        content:(
+            <ul className='list-disc pl-2'>
+                <li>+3 years as a Software Engineer</li>
+                <li>+2 years as a Full-Stack web development at ZEOUR LTD, London, UK</li>
+            </ul>
+        )
+    },
+]
+const AboutSection = () => {
+    const[tab,setTab] = useState("skills");
+    const[isPending,startTransition] = useTransition();
+
+    const handleTabChange = (id)=>{
+        startTransition(()=>{
+            setTab(id);
+        })
+    }
+  return (
+    <section className='text-white'>
+        <div className='md:grid md:grid-cols-2 gap-8 py-8 px=4 xl:gap-16 sm:py-16 xl:px-16'>
+            <Image src="/images/About-image.png" width={500} height={500}/>
+            <div className='mt-4 md:mt-0 text-left flex-col h-full'>
+                <h2 className='tetxl font-bold text-white mb-4'>
+                    About Me
+                </h2>
+                <p className='text-base me:text-lg'>
+                    Results-driven Software Engineer with expertise 
+                    in full-stack development. Proﬁcient in multiple 
+                    programming languages and dedicated to delivering 
+                    high-quality, scalable solutions. Adept at 
+                    collaborating in cross-functional teams to meet 
+                    project goals. Passionate about staying current 
+                    with emerging technologies and contributing 
+                    innovative ideas. Seeking opportunities to apply 
+                    skills in a dynamic and challenging environment.
+                </p>
+                <div className='flex flex-row justify-start mt-8'>
+                    {
+                        TAB_DATA.map((data,index)=>(
+                            <TabButton key={index} selectTab={()=>handleTabChange(data.id)} active={tab===data.id} >{" "}{data.title}{" "}</TabButton>
+                        ))
+                    }
+
+                </div>
+                <div className='mt-8'>
+                    {TAB_DATA.find((data)=>data.id===tab).content}
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default AboutSection
---
+++ app/components/ProjectsSection.jsx
@@ -0,0 +1,70 @@
+"use client"
+import React, { useRef, useState } from 'react'
+import ProjectCard from './ProjectCard'
+import ProjectTag from './ProductTag';
+import { motion, useInView } from "framer-motion";
+
+
+const projectsData = [
+    {
+        id:1,
+        title:'Ruma8',
+        description:"MERN Stack Food Order Platform",
+        image:"/images/projects/Ruma8.png",
+        tag:['All','Web'],
+        git:'https://github.com/Rami9999/MERN-Stack-food-ordering-application-frontend',
+        live:'https://ruma-food-ordering-application-frontend.onrender.com/',
+    },
+]
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+  return (
+    <section >
+        <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
+            My Projects
+        </h2>
+        <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+            <ProjectTag
+            onClick={handleTagChange}
+            name="All"
+            isSelected={tag === "All"}
+            />
+            <ProjectTag
+            onClick={handleTagChange}
+            name="Web"
+            isSelected={tag === "Web"}
+            />
+        </div>
+        <ul ref={ref} className='grid md:grid-cols-3 gap-8 md:gap-12'>
+            {filteredProjects.map((project,index)=>(
+                <motion.li
+                  key={index}
+                  variants={cardVariants}
+                  initial="initial"
+                  animate={isInView ? "animate" : "initial"}
+                  transition={{ duration: 0.3, delay: index * 0.4 }}
+                >
+                  <ProjectCard key={index} gitUrl={project.git} previewUrl={project.live} title={project.title} description={project.description} imgUrl={project.image} />
+                </motion.li>
+            ))}
+        </ul>
+    </section>
+  )
+}
+
+export default ProjectsSection
