7fe393ee95128fde4e500bf30d0a9554cb6eb2e1
Rami Abo Ajeeb <rami@zeour.co.uk>
2024-10-31 16:18:43 +0400

    deployment bugs | 31/10/2024

--- app/components/HeroComponent.jsx
+++ app/components/HeroComponent.jsx
@@ -15,7 +15,7 @@ const HeroComponent = () => {
             transition={{ duration: 0.5 }}
             className='col-span-7 place-self-center text-center sm:text-left'>
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 '>Hello, I'm {" "} </span>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 '>Hello, I&apos;m{" "} </span>
                     <br></br>
                     <TypeAnimation
                     sequence={[
